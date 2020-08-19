9715a05f77e42b5b6f57da79185fcdb0b972f890
SperZ <ZachSperka@gmail.com>
2020-08-18 22:34:17 -0400

    added Error message if note could not be could not be created, added alert if note was created.

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,5 +1,7 @@
 ﻿using ElevenNote.Data;
 using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,9 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            NoteService service = CreateNoteService();
+            var model = service.GetNotes();
+
             return View(model);
         }
 
@@ -30,11 +34,28 @@ namespace ElevenNote.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
-            {
+            if (!ModelState.IsValid) return View(model);
 
+
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created";
+                return RedirectToAction("Index");
             }
+
+            ModelState.AddModelError("", "Note  could not be created");
+
             return View(model);
+           
+        }
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
         }
     }
 }
